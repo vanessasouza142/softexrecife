f57f3b48d19434d431dd535aaa63a608daaa3bcb
VanessaSouza142 <108378034+VanessaSouza142@users.noreply.github.com>
2022-08-22 22:10:01 -0300

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -3,7 +3,8 @@
 # nota_1: Primeira nota;
 # nota_2: Segunda nota;
 # faltas: Número de faltas;
-# O programa lerá esse arquivo e criará duas colunas. A primeira coluna será “media”, que terá a média das duas notas do aluno. A segunda será “situacao”, com os valores: APROVADO ou REPROVADO.
+# O programa lerá esse arquivo e criará duas colunas. A primeira coluna será “media”, que terá a média das duas notas do aluno. A segunda será “situacao”, com os 
+# valores: APROVADO ou REPROVADO.
 # O aluno que tiver mais de 5 faltas ou possuir média menor que sete, será reprovado. O programa deverá salvar esse novo dataframe com o nome “alunos_situacao.csv”.
 # Por fim, o programa deverá mostrar na tela:
 # - o maior número de faltas;
