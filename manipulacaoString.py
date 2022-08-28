8b94edb0c9d8a4cd0735435492c98716003450b5
VanessaSouza142 <108378034+VanessaSouza142@users.noreply.github.com>
2022-08-28 10:37:03 -0300

    Create manipulacaoString.py

---
+++ manipulacaoString.py
@@ -0,0 +1,14 @@
+#Estruture três códigos, os quais devem conter uma função de manipulação de string que obtenha resultado.
+
+nome1 = "Pernambuco"
+nomeLetraMaiuscula = nome1.upper()
+print (nomeLetraMaiuscula)
+
+
+nome2 = "Bahia"
+nomeLetraMinuscula = nome2.lower()
+print (nomeLetraMinuscula)
+
+
+nome3 = "Sergipe"
+print (len(nome3))
