ba1d1a21bc781ba4c124a34013c15782577e7200
VanessaSouza142 <108378034+VanessaSouza142@users.noreply.github.com>
2022-09-07 21:57:49 -0300

    Update node.js

--- node.js
+++ node.js
@@ -1,3 +1,20 @@
 /*Escolha algum código executável em JavaScript feito em atividades passadas e o coloque no Node.js. Para isso, você deve ter o Node.js instalado. 
 Após fazer essa migração, coloque seu código no arquivo "index.js", teste e tire um print.
 */
+
+/* CODIGO */
+
+function nota (n1, n2, n3){
+
+    let nota1 = n1
+    let nota2 = n2
+    let nota3 = n3
+    let resultado = ((nota1+nota2+nota3)/3 >= 7) ? "Aprovado" : "Reprovado"
+    console.log("Você está: " + resultado)
+}        
+
+console.log(nota(7,8,9))
+
+/* PRINT DO CODIGO EXECUTADO NO NODE */
+
+
