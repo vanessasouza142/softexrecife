fe412428ec5399de6b93c47409da3b1a0fcee9f6
VanessaSouza142 <108378034+VanessaSouza142@users.noreply.github.com>
2022-09-11 20:38:49 -0300

    Update banco.js

--- banco.js
+++ banco.js
@@ -8,3 +8,31 @@ Observações:
 - para o saque, você deverá passar um valor como parâmetro e subtraí-lo no saldo final do objeto;
 - o número da conta deve retornar o número da conta.
 */
+
+function banco (conta, saldo, tipodeConta, agencia) {
+    this.conta = conta
+    this.saldo = saldo
+    this.tipodeConta = tipodeConta
+    this.agencia = agencia
+
+    buscarSaldo = function valorAtualSaldo() {
+        return (this.saldo)
+    }
+    deposito = function deposito (valorDeposito) {
+        return (this.saldo + valorDeposito)
+    }
+    saque = function (valorSaque) {
+       return (this.saldo - valorSaque)
+    }
+    numeroConta = function () {
+        return (this.conta)
+    }
+}
+
+let minhaConta = new banco ("60500", 2000, "contacorrente", "0123")
+
+console.log(minhaConta)
+console.log(minhaConta.buscarSaldo()) 
+console.log(minhaConta.deposito(1000))
+console.log(minhaConta.saque(300))
+console.log(minhaConta.numeroConta())
