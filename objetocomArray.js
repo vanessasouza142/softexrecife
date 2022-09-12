6424d01b6f6a3c3e714c789c93bf6f888c36b376
VanessaSouza142 <108378034+VanessaSouza142@users.noreply.github.com>
2022-09-11 22:42:23 -0300

    Update objetocomArray.js

--- objetocomArray.js
+++ objetocomArray.js
@@ -5,3 +5,29 @@ Desenvolva um código e crie nele:
 - um array de tamanho três no mínimo;
 - duas funções, a primeira lista as propriedades do objeto e a segunda, os elementos do array.
 */
+
+
+// Objeto e funcao For In
+let aluno = {
+    nome: "Juliana"
+    idade: 15
+    serie: 9
+}
+
+
+function listaPropriedades(aluno) {
+    for (const propriedade in aluno) {
+    console.log(`${propriedade}: ${aluno[propriedade]}`)
+    }
+}
+
+
+
+// Array e funcao For of
+const cores = ["vermelho", "azul", "amarelo"]
+
+function listaElementosArray(cores){
+    for (const elemento of cores) {
+        console.log(`${elemento}`) 
+    }
+}
