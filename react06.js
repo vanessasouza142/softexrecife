3fb6faa0975cd4ce71e2546f18c42eba9fc86753
VanessaSouza142 <108378034+VanessaSouza142@users.noreply.github.com>
2022-11-05 21:55:41 -0300

    Update react06.js

--- react06.js
+++ react06.js
@@ -1,1 +1,15 @@
+import './App.css';
+import React from 'react';
+import Routes from "./Routes";
 
+function App (){
+  return(
+    <div>
+      <header class='App-header'>
+        <Routes/>
+      </header>
+    </div>  
+  );
+}
+
+export default App;
