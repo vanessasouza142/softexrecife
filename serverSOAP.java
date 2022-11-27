46d9b4f0cb074123aa2bb69082f28fb40a74714d
VanessaSouza142 <108378034+VanessaSouza142@users.noreply.github.com>
2022-11-27 10:59:17 -0300

    Update serverSOAP.java

--- serverSOAP.java
+++ serverSOAP.java
@@ -1,1 +1,17 @@
+package app;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+
+@WebService
+@SOAPBinding(style = Style.RPC)
+public interface bancoServer {
+  
+  @WebMethod string pegarBoleto (String codigoBarras);
+  @WebMethod string criarBoleto (float valor, String cpfClient);
+  @WebMethod boolean verificarPagamento (String codigoBarras);
+  @WebMethod boolean cancelarBoleto (String codigoBarras);
+}
 
